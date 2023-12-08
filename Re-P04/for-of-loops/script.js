d5a15528dfb93ca9f30730d0612c4b0d28fb9971
hatsune <hatsune.k.pc8010@gmail.com>
2023-12-08 23:37:04 +0900

    20231208

---
+++ Re-P04/for-of-loops/script.js
@@ -0,0 +1,42 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+
+//Q1
+
+/**
+ * @param {Array<number>} ??? - 数値型の要素を持つ配列
+ * @returns {number} 与えられた配列のすべての数字の合計
+ */
+
+function sumArray(arraynumber) {
+    
+    for const A of arraynumber
+
+
+  }
+  
+  actual = sumArray([1, 2, 3, 4]);
+  expected = 10;
+  
+  if (actual === expected) {
+    console.log("Test PASSED.");
+  } else {
+    console.error("Test FAILED. Keep trying!");
+    console.group("Result:");
+    console.log("  actual:", actual);
+    console.log("expected:", expected);
+    console.groupEnd();
+  }
+  
+  // さらにテストを書きましょう。
+  
+
+
+
+
+
+
+
+
+
